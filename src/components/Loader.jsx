f07f2e0bdc2e5c4a9fc6ca505508d2d698cf93c8
trongdv <trongdv@glotechvn.com>
2026-02-25 15:02:27 +0700

    update code

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import './Loader.css';
 
 const Loader = ({ text = "Đang ngưng tụ linh khí..." }) => {
   return (
-    <div className="tu-tien-loader-container">
+    <div className="immortality-loader-container">
       <div className="magic-array">
         <div className="outer-ring"></div>
         <div className="middle-ring">
